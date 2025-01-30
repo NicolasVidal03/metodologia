51a4d03f96cb280e8b9d1a46f8a48427bfee76c3
NicolasVidal03 <nicolas.vidal@ucb.edu.bo>
2025-01-29 20:58:24 -0400

    pequeño arreglo

--- frontend/src/components/PerfilPerro/PerfilPerro.js
+++ frontend/src/components/PerfilPerro/PerfilPerro.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import './PerfilPerro.css';
 import { FaArrowLeft } from 'react-icons/fa';
